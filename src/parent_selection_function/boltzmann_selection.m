64dd36588dba0f50589fd76c94e0f64565d4dc9b
gonzalo <gibarsin@itba.edu.ar>
2017-05-27 15:43:46 -0300

    add population mean in boltzmann selection method

--- src/parent_selection_function/boltzmann_selection.m
+++ src/parent_selection_function/boltzmann_selection.m
@@ -1,11 +1,15 @@
 function parents = boltzmann_selection( population, K, T )
     
     population_fitness = calculate_population_fitness(population);
-    expected_values = zeros(length(population));
+    len = length(population);
+    expected_values = zeros(len);
     
-    for i = 1:length(population)
+    
+    mean = population_mean(population_fitness);
+    
+    for i = 1:len
         fitness_i = population_fitness(i).fitness;
-        expected_values(i) = exp(fitness_i / T) / expected_value_poplation;
+        expected_values(i) = exp(fitness_i / T) / mean;
     end
     
     % Sort the expected values from high to low
@@ -24,7 +28,7 @@ function parents = boltzmann_selection( population, K, T )
     for i = 1:K
         r_i = randoms(i);
         
-        for j = 1:length(population)
+        for j = 1:len
             % Get the index of the next highest expected value
             index = idx_sorted_expected_values(j);
             % Get the expected value of the original array
@@ -41,6 +45,17 @@ function parents = boltzmann_selection( population, K, T )
     end
     
     parents = population(indexes);
+end
+
+function population_mean = population_mean(population_fitness)
+    len = length(population);    
+    population_mean = 0;
+    
+    for i = 1:len
+        fitness_i = population_fitness(i).fitness;
+        population_mean = population_mean + exp(fitness_i / T);
+    end
+    population_mean = population_mean / len;
 end
 
 function cum_fitness = get_cum_fitness(population_fitness)
