6c4a605c8a9bbf8b27358bf5941ec18186f61fa6
mati <matimercado06@hotmail.com>
2017-06-01 00:50:59 -0300

    Refactor Mutation

--- src/crossover_function/mutate_attributes.m
+++ src/crossover_function/mutate_attributes.m
@@ -2,37 +2,20 @@ function [height, items_set] = mutate_attributes(attributes, ...
     mutation_probability, all_data)
     config = get_config('main');
     min_height = config.min_height;
-    max_height = config.max_height;
-    
+    max_height = config.max_height;    
     [height, items_set] = unwrap_cell_attributes(attributes);
+    file_names = {'boots', 'chests', 'gloves', 'helmets', 'weapons'};
+    items_names = {'boots', 'chest', 'gloves', 'helmet', 'weapon'};
     
     if rand() < mutation_probability
-        height = rand_double(min_height, max_height);
+        % Only a single attribute (chosen randomly) will be mutated
+        attributes_index = randi(length(attributes));
+        if(attributes_index == 1)
+            height = rand_double(min_height, max_height);
+        else
+            file_name = file_names{attributes_index-1};
+            item_name = items_names{attributes_index-1};
+            items_set.(item_name) = load_sample_item(all_data, file_name);
+        end
     end    
-    items_set = mutate_items_set(all_data, items_set, mutation_probability);
-end
-
-%% Private Functions
-function items_set = mutate_items_set(all_data, ...
-    items_set, mutation_probability)
-
-    items_set.boots = mutate_item(all_data, 'boots', ...
-        items_set.boots, mutation_probability);
-    items_set.chest = mutate_item(all_data, 'chests', ...
-        items_set.chest, mutation_probability);
-    items_set.gloves = mutate_item(all_data, 'gloves', ...
-        items_set.gloves, mutation_probability);
-    items_set.helmet = mutate_item(all_data, 'helmets',...
-        items_set.helmet, mutation_probability);
-    items_set.weapon = mutate_item(all_data, 'weapons', ...
-        items_set.weapon, mutation_probability);
-end
-
-function item = mutate_item(all_data, item_name, old_item, ...
-    mutation_probability)
-    if rand() < mutation_probability
-        item = load_sample_item(all_data, item_name);
-    else
-        item = old_item;
-    end
 end
